5616c18788ee9c85f2dc844b6e462571b37e8e2a
sloanstewart <sloan_stewart@hotmail.com>
2017-07-11 18:07:28 -0400

    PUT/:id now updates a post

--- server.js
+++ server.js
@@ -82,6 +82,37 @@ app.post('/posts', jsonParser, (req, res) => {
     });
 });
 
+app.put('/posts/:id', jsonParser, (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    res.status(400).json({message: message});
+  }
+
+  // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const updateableFields = ['title', 'content', 'author'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Post
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(
+      post => res.status(200).json(post.apiRepr()).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
 // runServer for testing
 let server;
 
